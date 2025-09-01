d287fd8a6063e9c7e0916cdd63a3f87c17d35c53
Ahmed Asar <a7med3sar22@gmail.com>
2025-09-01 19:35:32 +0300

    Fix: app reviews and add permissions

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from reviews.models import Review
 
-# Create your views here.
+
+class ReviewListView(ListView):
+    """
+    A view to display a list of all reviews.
+    """
+
+    model = Review
+    template_name = "reviews/review_list.html"
+    context_object_name = "reviews"
+
+
+class ReviewDetailView(DetailView):
+    """
+    A view to display the details of a single review.
+    """
+
+    model = Review
+    template_name = "reviews/review_detail.html"
+    context_object_name = "review"
